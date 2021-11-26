ed4b7f4f2e65ebf285445d0cc5b0a82b59e7a502
hlemarchant <hugo.lemarchant@insa-cvl.fr>
2021-11-26 11:20:59 +0900

    update

--- auto_evaluate.sh
+++ auto_evaluate.sh
@@ -4,10 +4,10 @@ StringVal="doexp5 dosq4015"
 
 for v in {0..4}; do
   for ckpt in $StringVal; do
-    python3 Eval_robustness.py --version "$v" --ckpt "$ckpt"
+    python3 Eval_robustness.py --data "$1" --version "$v" --ckpt "$ckpt"
   done
-  python3 Eval_robustness.py --version "$v"
-  python3 Eval_robustness.py --version "$v" -p
+  python3 Eval_robustness.py --data "$1" --version "$v"
+  python3 Eval_robustness.py --data "$1" --version "$v" -p
 done
 
 python3 combine_eval_summaries.py
